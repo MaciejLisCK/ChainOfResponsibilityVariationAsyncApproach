c82312aae8b6244f0bbf8bb0ab5bfa5f5f55249f
Maciej Lis <accounts@maciejlis.com>
2024-05-10 20:43:08 +0200

    cleanup2

--- src/DashboardProcess.ts
+++ src/DashboardProcess.ts
@@ -2,7 +2,7 @@ import { BehaviorSubject, filter, map } from "rxjs";
 import { ProcesState } from "./ProcesState";
 
 
-export class DashboardProcess {
+export abstract class DashboardProcess {
     state$ = new BehaviorSubject(ProcesState.NotStarted);
 
     isShouldRunOrSkipState$ = this.state$.pipe(map(s => s === ProcesState.ShouldRunOrSkip));
@@ -17,7 +17,19 @@ export class DashboardProcess {
     skippedState$ = this.isSkippedState$.pipe(filter(v => v));
     doneOrSkippedState$ = this.isDoneOrSkippedState$.pipe(filter(v => v));
     
+    constructor() {
+        this.shouldRunOrSkipState$.subscribe({ next: () => this.shouldRunOrSkip() });
+        this.runningState$.subscribe({ next: () => this.running() });
+        this.skippedState$.subscribe({next: () => this.skipped() })
+        this.doneState$.subscribe({ next: () => this.done() });
+    }
+
     start(): void {
         this.state$.next(ProcesState.ShouldRunOrSkip);
     }
+
+    abstract shouldRunOrSkip(): void
+    abstract running(): void
+    abstract skipped(): void
+    abstract done(): void
 }
--- src/Survey4.ts
+++ src/Survey4.ts
@@ -4,10 +4,6 @@ import { DashboardProcess } from "./DashboardProcess";
 export default class Survey4 extends DashboardProcess {
     constructor() {
         super();
-        this.shouldRunOrSkipState$.subscribe({ next: () => this.shouldRunOrSkip() });
-        this.runningState$.subscribe({ next: () => this.running() });
-        this.skippedState$.subscribe({ next: () => this.skipped() })
-        this.doneState$.subscribe({ next: () => this.done() });
     }
 
     shouldRunOrSkip(): void {
