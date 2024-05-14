285d150f15f57cdc4bf315da83af22201ad137c1
Maciej Lis <accounts@maciejlis.com>
2024-05-14 23:10:58 +0200

    enhancements

--- src/DashboardProcess.ts
+++ src/DashboardProcess.ts
@@ -1,21 +1,21 @@
-import { BehaviorSubject, filter, map, tap } from "rxjs";
+import { BehaviorSubject, distinctUntilChanged, filter, map, tap } from "rxjs";
 import { ProcesState } from "./ProcesState";
 
 
 export abstract class DashboardProcess {
     state$ = new BehaviorSubject(ProcesState.NotStarted);
 
-    isShouldRunOrSkipState$ = this.state$.pipe(map(s => s === ProcesState.ShouldRunOrSkip));
-    isRunningState$ = this.state$.pipe(map(s => s === ProcesState.Running));
-    isDoneState$ = this.state$.pipe(map(s => s === ProcesState.Done));
-    isSkippedState$ = this.state$.pipe(map(s => s === ProcesState.Skipped));
-    isDoneOrSkippedState$ = this.state$.pipe(map(s => s === ProcesState.Done || s === ProcesState.Skipped));
+    isShouldRunOrSkipState$ = this.state$.pipe(map(s => s === ProcesState.ShouldRunOrSkip), distinctUntilChanged());
+    isRunningState$ = this.state$.pipe(map(s => s === ProcesState.Running), distinctUntilChanged());
+    isDoneState$ = this.state$.pipe(map(s => s === ProcesState.Done), distinctUntilChanged());
+    isSkippedState$ = this.state$.pipe(map(s => s === ProcesState.Skipped), distinctUntilChanged());
+    isDoneOrSkippedState$ = this.state$.pipe(map(s => s === ProcesState.Done || s === ProcesState.Skipped), distinctUntilChanged());
 
-    shouldRunOrSkipState$ = this.isShouldRunOrSkipState$.pipe(filter(v => v));
-    runningState$ = this.isRunningState$.pipe(filter(v => v));
-    doneState$ = this.isDoneState$.pipe(filter(v => v));
-    skippedState$ = this.isSkippedState$.pipe(filter(v => v));
-    doneOrSkippedState$ = this.isDoneOrSkippedState$.pipe(filter(v => v));
+    shouldRunOrSkipState$ = this.isShouldRunOrSkipState$.pipe(filter(Boolean));
+    runningState$ = this.isRunningState$.pipe(filter(Boolean));
+    doneState$ = this.isDoneState$.pipe(filter(Boolean));
+    skippedState$ = this.isSkippedState$.pipe(filter(Boolean));
+    doneOrSkippedState$ = this.isDoneOrSkippedState$.pipe(filter(Boolean));
     
     constructor() {
         this.shouldRunOrSkipState$.subscribe({ next: () => this.shouldRunOrSkip() });
--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,10 @@ import { ProcessManager } from "./ProcessManager";
 const processManager = new ProcessManager()
 processManager.finished$.subscribe(() => console.log("All surveys are finished."))
 
+processManager.survey1.runningState$.subscribe((s) => console.log("Survey 1 state: ", s))
+
 processManager.start()
+
+setTimeout(() => {
+  console.log("Delayed for 1 second.");
+}, 1000);
